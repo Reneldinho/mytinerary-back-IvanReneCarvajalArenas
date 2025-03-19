a6c6af31c2618b7d9aa7ce77b13b5d90c7ad13d2
Reneldinho <pibe1109@gmail.com>
2025-03-19 12:24:14 -0500

    Creando el modelo City.js

---
+++ models/City.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose"
+
+let collection = "cities"
+let schema = Schema({
+    cityName: { type: String, required: true },
+    cituCountry: { type: String, required: true },
+    cityPhoto: { type: String, required: true },
+    cityDescription: { type: String, required: true },
+    localCurrency: { type: String, required: true },
+    importantRestaurants: { type: String, required: true },
+    importantAttractions: { type: String, required: true },
+    mainParks: { type: String, required: true },
+    notableTouristSities: { type: String, required: true }
+}, {
+    timestamps: true
+});
+
+let City = model(collection, schema);
+
+export default City;
